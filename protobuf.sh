af46bd27739fda32b66c3135cec41750376b0a0b
om-mks <murayama@mukei-soft.co.jp>
2021-08-31 11:50:00 +0900

    rename proto_src_dir and refactor.

--- protobuf.sh
+++ protobuf.sh
@@ -1,9 +1,9 @@
-readonly INTERFACE_SRC_DIR="../mks-ex-proto-interfaces/src/proto/"
+readonly PROTO_SRC_DIR="../mks-ex-proto-defs/src/proto/"
 
-for p in `find $INTERFACE_SRC_DIR -name '*.proto'` 
+for p in `find $PROTO_SRC_DIR -name '*.proto'` 
 do
- relative_path=`echo $p | sed "s@$INTERFACE_SRC_DIR@@g" | sed 's@^/@@'`
+ relative_path=`echo $p | sed "s@$PROTO_SRC_DIR@@g" | sed 's@^/@@'`
  echo "target file is $relative_path"
  protoc --dart_out=grpc:lib/ \
-        --proto_path=$INTERFACE_SRC_DIR $relative_path
+        --proto_path=$PROTO_SRC_DIR $relative_path
 done
